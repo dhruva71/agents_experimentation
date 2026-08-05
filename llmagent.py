a3b6e2f37d0e4616b37449e97dfa15c44e86e44a
Dhruvajyoti Sarma <onlinedhruva71@gmail.com>
2026-08-05 20:37:28 +0530

    Basic working version

---
+++ llmagent.py
@@ -0,0 +1,53 @@
+import os
+
+import yaml
+from openai import OpenAI
+from openai.types.chat import ChatCompletionMessage
+
+
+class LLMAgent:
+    def __init__(self, api_key: str, model_id: str = "deepseek/deepseek-v4-flash", prompts_yaml_path: str = 'prompts/prompts.yaml'):
+        self.api_key = api_key
+        self.model_id = model_id
+        print(f'Using model: {self.model_id}')
+
+        with open(prompts_yaml_path, 'r') as f:
+            config = yaml.safe_load(f.read())
+
+        if config is not None:
+            self.system_prompt = config["system_prompt_2"]
+        else:
+            # TODO log this better
+            print("No system_prompt provided")
+            self.system_prompt = ""
+
+        print(f'{self.system_prompt=}\n')
+
+        self.client = OpenAI(
+            base_url="https://openrouter.ai/api/v1",
+            api_key=os.getenv("OPENROUTER_API_KEY"),
+        )
+
+    def query_llm(self, user_query: str, reasoning_enabled: bool = False, **kwargs) -> ChatCompletionMessage:
+        """
+        Get response from model `model_id` for `user_query`.
+        :param user_query:
+        :param reasoning_enabled: Whether reasoning is enabled or not.
+        :return: JSON response
+        """
+        # noinspection bad-argument-type
+        response = self.client.chat.completions.create(
+            model=self.model_id,
+            messages=[
+                {"role": "system", "content": self.system_prompt},
+                {
+                    "role": "user",
+                    "content": user_query
+                }
+            ],
+            extra_body={"reasoning": {"enabled": reasoning_enabled}},
+            **kwargs
+        )
+
+        response = response.choices[0].message
+        return response
---
+++ main.py
@@ -0,0 +1,24 @@
+import json
+import os
+
+from dotenv import load_dotenv
+
+import llmagent
+
+load_dotenv()
+
+models = ["openai/gpt-5.6-luna", "nvidia/nemotron-3-ultra-550b-a55b:free", "deepseek/deepseek-v4-flash"]
+selected_model_index = 0
+
+api_key = os.getenv("OPENROUTER_API_KEY")
+if not api_key:
+    raise ValueError("API key not set")
+llm = llmagent.LLMAgent(api_key=api_key, model_id=models[selected_model_index])
+
+query = "Display budget vs actual for FY25 for department IT"
+response = llm.query_llm(user_query=query, max_completion_tokens=100, stop=['Observation'])
+
+print(response)
+if response.content is not None: # split for readability
+    if "action" in response.content:
+        print("Response contains action")
