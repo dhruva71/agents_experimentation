eed56ea8ac1ecdab49796bf6ef9024fecb221f13
Dhruvajyoti Sarma <onlinedhruva71@gmail.com>
2026-08-21 16:15:02 +0530

    Added toolbox class, now to add ways to inject tools into the prompt

--- main.py
+++ main.py
@@ -4,41 +4,44 @@ import os
 from dotenv import load_dotenv
 
 import llmagent
+import toolbox
 
-logger = logging.getLogger(__name__)
-logging.basicConfig(
-    level=logging.DEBUG,
-    format="%(asctime)s %(name)s %(levelname)s: %(message)s"
-)
+if __name__ == '__main__':
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="%(asctime)s %(name)s %(levelname)s: %(message)s"
+    )
 
-logger.info("Loading environment variables")
-load_dotenv()
+    logger.info("Loading environment variables")
+    load_dotenv()
 
-models = ["openai/gpt-5.6-luna", "nvidia/nemotron-3-ultra-550b-a55b:free", "deepseek/deepseek-v4-flash",
-          "stealth/ox-alpha"]
-selected_model_index = -1
+    models = ["openai/gpt-5.6-luna", "nvidia/nemotron-3-ultra-550b-a55b:free", "deepseek/deepseek-v4-flash",
+              "stealth/ox-alpha"]
+    selected_model_index = -1
 
-api_key = os.getenv("OPENROUTER_API_KEY")
-if not api_key:
-    raise ValueError("API key not set")
-agent = llmagent.LLMAgent(api_key=api_key, model_id=models[selected_model_index])
+    # Intialize agent
+    api_key = os.getenv("OPENROUTER_API_KEY")
+    if not api_key:
+        raise ValueError("API key not set")
+    agent = llmagent.LLMAgent(api_key=api_key, model_id=models[selected_model_index])
 
-query = "Display budget vs actual for FY25 Q4 for department IT"
-logger.info(f"Executing with query: {query}")
-logger.debug(f"Selected model: {models[selected_model_index]}")
-reasoning_enabled = True
-response = agent.execute_query(user_query=query, reasoning_enabled=reasoning_enabled, max_completion_tokens=1000,
-                               stop=['Observation'], logprobs=True,
-                               top_logprobs=10)
+    query = "Display budget vs actual for FY25 Q4 for department IT"
+    logger.info(f"Executing with query: {query}")
+    logger.debug(f"Selected model: {models[selected_model_index]}")
+    reasoning_enabled = True
+    response = agent.execute_query(user_query=query, reasoning_enabled=reasoning_enabled, max_completion_tokens=1000,
+                                   stop=['Observation'], logprobs=True,
+                                   top_logprobs=10)
 
-logger.debug(response)
-logprobs = agent.process_logprobs()
-content = response.choices[0].message.content
+    logger.debug(response)
+    logprobs = agent.process_logprobs()
+    content = response.choices[0].message.content
 
-if content is not None:  # split for readability
-    if "action" in content:
-        logger.debug("Response contains action")
-        logger.debug(f'{content=}')
-        action = agent.extract_action()
-        logger.info(f'{action=}')
-        print(f'Received action: {action}')
+    if content is not None:  # split for readability
+        if "action" in content:
+            logger.debug("Response contains action")
+            logger.debug(f'{content=}')
+            action = agent.extract_action()
+            logger.info(f'{action=}')
+            print(f'Received action: {action}')
--- toolbox.py
+++ toolbox.py
@@ -1,2 +1,37 @@
+from typing import Callable
+
+
 class Toolbox:
     def __init__(self):
+        self.tools = []
+
+    def tool(self, function: Callable):
+        self.tools.append(function)
+
+    def list_tools(self, json_mode: bool=False):
+        if not json_mode:
+            for tool in self.tools:
+                print(f'Tool name: {tool.__name__}\nDescription: {tool.__doc__}')
+        else:
+            json_tools = []
+            for tool in self.tools:
+                json_tools.append({
+                    "name": tool.__name__,
+                    "description": tool.__doc__
+                })
+            return json_tools
+
+if __name__ == "__main__":
+    toolbox = Toolbox()
+
+    @toolbox.tool
+    def add(a, b):
+        """Add two numbers and returns the result."""
+        return a + b
+
+    @toolbox.tool
+    def subtract(a, b):
+        """Subtract two numbers and returns the result."""
+        return a - b
+
+    print(toolbox.list_tools(json_mode=True))
