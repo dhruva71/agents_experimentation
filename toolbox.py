1f40023403829f49cdbceb4b602666dbf728a354
Dhruvajyoti Sarma <onlinedhruva71@gmail.com>
2026-08-21 16:46:50 +0530

    Updated parameter resolution

--- toolbox.py
+++ toolbox.py
@@ -10,26 +10,27 @@ class Toolbox:
         self.tools.append(function)
         return function
 
-    def list_tools(self, json_mode: bool = False):
-        if not json_mode:
-            for tool in self.tools:
-                print(f'Tool name: {tool.__name__}\nDescription: {tool.__doc__}')
-        else:
-            json_tools = []
-            for tool in self.tools:
-                parameters = []
-                tool_signature = inspect.signature(tool)
-                for name, param in tool_signature.parameters.items():
-                    parameters.append({
-                        "name": name,
-                        "type": param.annotation.__name__
-                    })
-                json_tools.append({
-                    "name": tool.__name__,
-                    "description": tool.__doc__,
-                    "parameters": parameters
-                })
-            return json_tools
+    def list_tools(self) -> list:
+        def process_param_type(parameter_type: str):
+            if parameter_type == '_empty':
+                return 'any'
+            return parameter_type
+
+        tools_json = []
+        for tool in self.tools:
+            parameters = []
+            tool_signature = inspect.signature(tool)
+            for name, param in tool_signature.parameters.items():
+                param_type = param.annotation.__name__
+                parameters.append({name: {
+                    "type": process_param_type(param_type)
+                }})
+            tools_json.append({
+                "name": tool.__name__,
+                "description": tool.__doc__,
+                "parameters": parameters
+            })
+        return tools_json
 
 
 if __name__ == "__main__":
@@ -54,4 +55,4 @@ if __name__ == "__main__":
         return a * b
 
 
-    print(toolbox.list_tools(json_mode=True))
+    print(toolbox.list_tools())
